992a9a37b305e14eed5c08f6f6d6bb872737a9d5
constantijn <constantijn@vanhartesveldt.nl>
2022-04-26 08:59:27 +0200

    added home

--- components/pages/home/home.jsx
+++ components/pages/home/home.jsx
@@ -1,47 +1,54 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import style from "./home.styled.js";
 
 export default function Home(props) {
   const [wantsprovider, setWantsprovider] = useState("wantprovider");
+  const { data: session, status } = useSession();
+  const router = useRouter()
 
-  const searchhandler = async (e) => {
-    console.log(e);
-    e.preventDefault();
+  const sumbithandler = (event) => {
+    event.preventDefault();
+    let data = "";
+    data += `provider=${wantsprovider}&`
+    data += `postcode=${event.target.postcode.value}&`
+    data += `type=${event.target.type.value}`
+    router.push(`/overview/?${data}`)
   }
 
   return (
     <>
       <style.Box>
         <style.ProviderBox>
-            
-          <style.ProviderCard active={() => !wantsprovider && true} onClick={() => setWantsprovider("wantprovider")}>
+          <style.ProviderCard
+            active={wantsprovider === "wantprovider" && true}
+            onClick={() => setWantsprovider("wantreceiver")}
+          >
             hulp aanbieder
           </style.ProviderCard>
 
-          <style.ProviderCard active={() => wantsprovider && true} onClick={() => setWantsprovider("wantreceiver")}>
+          <style.ProviderCard
+            active={wantsprovider === "wantreceiver" && true}
+            onClick={() => setWantsprovider("wantprovider")}
+          >
             hulp vrager
           </style.ProviderCard>
-
         </style.ProviderBox>
-        <style.Form color="orange" action="/overview">
-          {wantsprovider === "wantreceiver" ? (
-            <>
-              <select name="type">
-                <option value="Gespreksmaatje">Gespreksmaatje</option>
-                <option value="Hulp in huis">Hulp in huis</option>
-                <option value="Tuinhulp">Tuinhulp</option>
-              </select>
-              <input type="text" name="postcode" placeholder="postcode"/>
-              <input type="submit"/>
-            </>
-          ) : (
-            <>  
-              <h1>leg uit wat uw situatie is</h1>
-              <textarea name="kaas" type="text" placeholder="vul in"/>
-            </>
-          )}
+
+        <style.Form onSubmit={sumbithandler} color="orange" >
+              <style.Select name="type">
+                <option value="gespreksmaatje">Gespreksmaatje</option>
+                <option value="hulp_in_huis">Hulp in huis</option>
+                <option value="tuinhulp">Tuinhulp</option>
+              </style.Select>
+              <style.InputField
+                type="text"
+                name="postcode"
+                placeholder="postcode"
+              />
+              <style.InputField type="submit" />
         </style.Form>
       </style.Box>
     </>
--- components/pages/home/home.styled.js
+++ components/pages/home/home.styled.js
@@ -4,28 +4,45 @@ const Form = styled.form`
     background-color: orange;
 `
 const ProviderCard = styled.div`
-    background-color: ${props => props.active ? "blue" : "black"};
-    border: 1px solid black;
+    background-color: ${props => (props.active ? "white" : "orange")};
+    color: ${props => (props.active ? "gray" : "white")};
+    border: 1px solid white;
+    border-radius: 10px;
+    text-align:center;
+    padding-top: 5px;
     margin: 20px;
     height: 50px;
     width: 100px;
-    /* &:hover{
-        background-color: red;
-    } */
 `
 const Box = styled.div`
-    background-color: purple;
-    width: 50%;
+    background-color: orange;
+    width: 550px;
+    height: 150px;
     margin: auto;
     margin-top: 20%;
+    @media only screen and (max-width: 525px) {
+     width: 100%;
+    }
 `
 const ProviderBox = styled.div`
     display: flex;
+`
+
+const Select = styled.select`
+    margin-right: 18px;
+    width: 30%;
+`
+
+const InputField = styled.input`
+    margin-right: 18px;
+    width: ${props => (props.width || "30%")};
 `
 
 module.exports = {
     Form,
     ProviderCard,
     Box,
-    ProviderBox
+    InputField,
+    ProviderBox,
+    Select,
 }
